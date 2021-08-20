7143598439ff0bb388b49be7a8ac385c6a5ed31b
lightningsun <philnewberry@talk21.com>
2021-08-20 23:33:51 +0100

    Create main.cpp
    Created a C-style array. Created another C-style array, generated content for it with logic and then read said content out.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    const int numberOfCardsInDeck = 52;
+    
+    // Standard C Arrays
+    const char specialPlayingCards [4] = {'K','Q','J','A'};
+    
+    //Making a collection of numbered playing cards and then reading them out
+    int deckOfNumberedCards [numberOfCardsInDeck];
+    for (int i=1;i<=numberOfCardsInDeck;i++)
+    {
+        deckOfNumberedCards[i] = i;
+    }
+    
+    std::cout << "Numbered playing cards:" << std::endl; 
+    for (int i=1;i<=numberOfCardsInDeck;i++) 
+    {
+        std::cout << deckOfNumberedCards[i];
+        if (i<numberOfCardsInDeck) std::cout << ", ";
+        else std::cout << ".";
+    }
+    
+    return 0;
+    
+}
