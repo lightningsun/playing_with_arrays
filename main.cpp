60d5443150aed58f0e9b5da5cd69f2423fafe63d
Lightningsun <philnewberry@talk21.com>
2021-08-21 15:34:27 +0100

    Tidy up of code to make it more presentable and modular (can comment functions out when there are too many of them)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main()
+void SpecialTypesOfCards() 
 {
     const int numberOfCardsInDeck = 52;
     const int numberOfSpecialTypesOfCards = 4;
@@ -18,22 +18,37 @@ int main()
     }
 
     std::cout << std::endl << std::endl;
+}
 
+void MakingNumberedCardsAndReadingThemOut()
+{
     // Making a collection of numbered cards and then reading them out
-    int deckOfNumberedCards [numberOfCardsInDeck];
-    for (int i=1;i<=numberOfCardsInDeck;i++)
+    int totalNumberedCards = 52; // This can be hard-code changed to alter deck size
+    
+    int deckOfNumberedCards [totalNumberedCards]; // Making array the size of total numbered cards
+    
+    // Defining all cards within deckOfNumberedCards to their specific number
+    for (int i=1;i<=totalNumberedCards;i++)
     {
         deckOfNumberedCards[i] = i;
     }
     
+    // Reading the cards out (cout'ing)
     std::cout << "Numbered playing cards:" << std::endl; 
-    for (int i=1;i<=numberOfCardsInDeck;i++) 
+    for (int i=1;i<=totalNumberedCards;i++) 
     {
         std::cout << deckOfNumberedCards[i];
-        if (i<numberOfCardsInDeck) std::cout << ", ";
+        if (i<totalNumberedCards) std::cout << ", ";
         else std::cout << ".";
     }
-    
+}
+
+int main()
+{
+    //Run all the different experiments (comment out if you only want specifics to run)
+    SpecialTypesOfCards();
+    MakingNumberedCardsAndReadingThemOut();
+
     std::cout << std::endl; // Keep this for a clean closing output in terminal
 
     return 0;
