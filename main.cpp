5847f162320aa439c23924fb57f83b1ec904faa2
Lightningsun <philnewberry@talk21.com>
2021-08-21 15:14:18 +0100

    Added a card type reading (declaring and reading c-style arrays)

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 int main()
 {
     const int numberOfCardsInDeck = 52;
+    const int numberOfSpecialTypesOfCards = 4;
     
-    // Standard C Arrays
-    const char specialPlayingCards [4] = {'K','Q','J','A'};
-    
-    //Making a collection of numbered playing cards and then reading them out
+    // Declaring and Defining the C-Style Arrays (Defining special types of playing cards)
+    const char specialPlayingCardsAcronym [numberOfSpecialTypesOfCards] = {'K','Q','J','A'}; // 2D array
+    const char specialPlayingCardsFullName [numberOfSpecialTypesOfCards][6] = {"King", "Queen", "Jack", "Ace"}; //2D array with defined length
+
+    // Reading the C-Style Arrays (Reading the types of special playing cards)
+    std::cout << "The type of special playing cards are: " << std::endl;
+    for (int i = 0; i < numberOfSpecialTypesOfCards; i++)
+    {
+        std::cout << specialPlayingCardsAcronym[i] << " which is also known as " 
+        << specialPlayingCardsFullName[i] << std::endl;
+    }
+
+    std::cout << std::endl << std::endl;
+
+    // Making a collection of numbered cards and then reading them out
     int deckOfNumberedCards [numberOfCardsInDeck];
     for (int i=1;i<=numberOfCardsInDeck;i++)
     {
